e77b4500e4558273971d22159664fec9d20e2e92
ayushisharma678 <ayushisharma67815@gmail.com>
2026-08-30 02:30:22 +0530

    Updated Level2, HoldPlate script, and Physics2D collision matrix settings

--- Assets/Scripts/Interactables/HoldPlate.cs
+++ Assets/Scripts/Interactables/HoldPlate.cs
@@ -51,6 +51,9 @@ public class HoldPlate : MonoBehaviour, IInteractable
             
             var door = linkedObject.GetComponent<Door>();
             door?.Open();
+
+            var platform = linkedObject.GetComponent<MovingPlatform>();
+            platform?.Raise();
         }
     }
 
@@ -66,6 +69,9 @@ public class HoldPlate : MonoBehaviour, IInteractable
 
             var door = linkedObject.GetComponent<Door>();
             door?.Close();
+
+            var platform = linkedObject.GetComponent<MovingPlatform>();
+            platform?.Lower();
         }
     }
 }
